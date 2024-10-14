70c097759949941fffeb28d36335ca6965ed830d
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-10-14 14:30:41 +0200

    Ajustement visuel du code mineur

--- src/components/templates/MainTemplate.jsx
+++ src/components/templates/MainTemplate.jsx
@@ -5,19 +5,19 @@ const MainTemplate = ({ children }) => {
     const currentYear = new Date().getFullYear();
 
     return (
-    <div className="main-template">
-        <header className="main-header">
-        <h1>Mon Projet Films et Séries et d'Animés</h1>
-        </header>
-        
-        <main className="main-content">
-        {children}
-        </main>
-        
-        <footer className="main-footer">
-        <p>© {currentYear} Mon Projet Films, Séries et Animés. Tous droits réservés.</p>
-        </footer>
-    </div>
+        <div className="main-template">
+            <header className="main-header">
+                <h1>Mon Projet Films et Séries et d'Animés</h1>
+            </header>
+            
+            <main className="main-content">
+                {children}
+            </main>
+            
+            <footer className="main-footer">
+                <p>© {currentYear} Mon Projet Films, Séries et Animés. Tous droits réservés.</p>
+            </footer>
+        </div>
     );
 };
 
